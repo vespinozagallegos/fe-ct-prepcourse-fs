0fdb1e1c8dbd6f9f61bf62a233b453ee886f66f0
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-05-29 16:57:30 -0400

    homework resolution except exercise 10

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -3,5 +3,10 @@ function esFechaValida(fecha) {
     // Comprueba si este corresponde a una fecha válida.
     // Si es así, retorna true, sino retorna false.
     // Tu código:
+    if (fecha === new Date(fecha)) {
+      return true;
+      }else{
+      return false;
+    }
   }       
   module.exports = esFechaValida;
