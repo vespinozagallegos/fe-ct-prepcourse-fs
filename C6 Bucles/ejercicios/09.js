aad10ab136ee965b2919e72824fa41ac2edbe7dc
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-07-15 17:05:37 -0400

    homework resolution except exercise 22

--- C6 Bucles/ejercicios/09.js
+++ C6 Bucles/ejercicios/09.js
@@ -5,7 +5,7 @@ function conection(status) {
   // De lo contrario, presumimos que el usuario está "Offline".
   // Retornar el estado de conexión del usuario.
   // Tu código:
-  if (status == 1) {
+  if (status == 1)  {
     return "Online";
   }
   if (status == 2) {
